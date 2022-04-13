a26360c70829c67416a354b583e78c5633734d7c
Chris Jimas <christostzimas2000@gmail.com>
2022-04-13 17:58:29 +0300

    No log out function

---
+++ ADOPSEV1.1/ADOPSEV1.1/Data/myDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ADOPSEV1._1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADOPSEV1._1.Data
+{
+    public class myDbContext : DbContext
+    {
+        public myDbContext(DbContextOptions<myDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ ADOPSEV1.1/ADOPSEV1.1/Models/UserLogin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace ADOPSEV1._1.Models
+{
+    public class UserLogin 
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage ="username is required")]
+        public string username { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        
+        [Display(Name ="Remember me")]
+        public bool RememberMe { get; set; }
+
+        
+    }
+}
