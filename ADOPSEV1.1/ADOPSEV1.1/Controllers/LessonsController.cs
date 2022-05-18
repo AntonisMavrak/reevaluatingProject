34536b1a7e2cad367670a6fd043fb8958f5c0da0
Christos <christostzimas2000@gmail.com>
2022-05-19 01:44:15 +0300

    user connects without check

--- ADOPSEV1.1/ADOPSEV1.1/Controllers/LessonsController.cs
+++ ADOPSEV1.1/ADOPSEV1.1/Controllers/LessonsController.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using ADOPSEV1._1.Data;
 using ADOPSEV1._1.Models;
-using System.Security.Claims;
-using System.Web;
-using System.Security.Principal;
 
 namespace ADOPSEV1._1.Controllers
 {
@@ -25,20 +17,7 @@ namespace ADOPSEV1._1.Controllers
         // GET: Lessons
         public async Task<IActionResult> Index()
         {
-            string usr;
-            //var userId = User.Identity.GetUserId();
-            if (User.Identity.IsAuthenticated) { 
-               usr = User.Identity.Name;
-               var users = _context.users.Where(u => u.username == usr).Select(u => u.id).FirstOrDefault();
-                ViewBag.users = users;
-               return View(users);
-            }
-            //var UserId = System.Web.HttpContext.Current.User.Identity.Name;
-            //ViewData["UserId"] = UserId;
-            //WindowsIdentity user;
-            //user = HttpContext.Request.LogonUserIdentity;
-
-            //ApplicationBuilderUser applicationUser = await _userManager.GetUserAsync(User);
+            
             IEnumerable<Lesson> allLessons = _context.lessons;
             return View(allLessons);
         }
@@ -46,17 +25,21 @@ namespace ADOPSEV1._1.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Lesson obj)
+        public IActionResult Create(int lessonInput)
         {
+            int users;
+            string usr;
+            if (User.Identity.IsAuthenticated)
+            {
+                usr = User.Identity.Name;
+                users = _context.users.Where(u => u.username == usr).Select(u => u.id).FirstOrDefault();
+                _context.userConnectsSubjects.Add(new UserConntectsSubject { lessonId = lessonInput, userId = users });
+                _context.SaveChanges();
+            }
             IEnumerable<Lesson> allLessons = _context.lessons;
-            var lessonid = obj.id;
-            
-            _context.userConnectsSubjects.Add(new UserConntectsSubject { lessonId = 5, userId = 5 });
-            
-
 
             //_context.userConnectsSubjects.Add(new UserConntectsSubject { userId = 5, lessonId = lessonid.ToString() });
-            _context.SaveChanges();
+            
             return View("Index", allLessons);
         }
 
