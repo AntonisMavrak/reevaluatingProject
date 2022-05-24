08a6f2b90164fef5768ca5aca043d0455aaa6593
Christos <christostzimas2000@gmail.com>
2022-05-24 23:22:08 +0300

    Login branches from db, password confirm

--- ADOPSEV1.1/ADOPSEV1.1/Controllers/UserController.cs
+++ ADOPSEV1.1/ADOPSEV1.1/Controllers/UserController.cs
@@ -74,11 +74,18 @@ namespace ADOPSEV1._1.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(string first_name, string last_name, string email, string password, string username, int BranchId)
+        public IActionResult Register(string first_name, string last_name, string email, string password, int BranchId, string password_repeat)
         {
 
             if (ModelState.IsValid)
             {
+
+                if(!password.Equals(password_repeat))
+                {
+                    ViewBag.Branch = _db.branches.ToList();
+                    IEnumerable<User> objUserList = _db.users;
+                    return View(objUserList);
+                }
                 User user = new User();
                 user.first_name = HttpContext.Request.Form["first_name"].ToString();
                 user.last_name = HttpContext.Request.Form["last_name"].ToString();
@@ -86,7 +93,7 @@ namespace ADOPSEV1._1.Controllers
                 user.branchId = Int32.Parse(HttpContext.Request.Form["dropBranch"]);  //
                 string typedPass = HttpContext.Request.Form["password"].ToString();
                 user.password = HashFun.Hash(password);
-                user.username = HttpContext.Request.Form["username"].ToString();
+                user.username = HttpContext.Request.Form["first_name"].ToString() + HttpContext.Request.Form["last_name"].ToString();
                 user.role = 0;
 
                 _db.users.Add(user);
